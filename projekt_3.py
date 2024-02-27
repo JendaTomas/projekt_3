69966e70e201e5ebad6f63e90e3059f84c38719d
JendaTomas <146070450+JendaTomas@users.noreply.github.com>
2024-02-27 11:03:03 +0100

    Update projekt_3.py

--- projekt_3.py
+++ projekt_3.py
@@ -5,187 +5,199 @@ email: jenda.tomas@seznam.cz
 discord: jendatomas
 """
 
-from requests import get
+import requests
 from bs4 import BeautifulSoup as bs
 import csv
 import sys
 
 
-
-def kod_obci(rozdelene_html):
+def find_code(parsered_html):
     """
-    vyscrapuje kódy jednotlivých obcí
+    find the codes of individual municipalities
     """
-    cisla_obci= rozdelene_html.find_all("td", class_="cislo")
-    kody=list()
-    for i in cisla_obci:
-        kody.append(i.string)
-    return kody
-
-def jmena_obci(rozdelene_html): 
-    """
-    vyscrapuje jmena obci, ale z hlavni stranky
-    """
-    jmena_obci= rozdelene_html.find_all("td", {"class":"overflow_name"}) 
-    obce=list()                                              
-    for i in jmena_obci:
-        obce.append(i.string)
-    return obce
-
-def odkazy_vysledky(rozdelene_html):
-    """
-    vyscrapuje odkazy na stránky s výsledky voleb v obcích
-    """
-    vyber_odkazu= rozdelene_html.find_all("td", {"class":"cislo"})
-    odkazy= list()
-    for i in vyber_odkazu:
-        odkaz = i.find_all("a")
-        for i in odkaz:
-            odkazy.append(i["href"])
-    spojeni=list()
-    for i in odkazy: 
-        spojeni.append("https://volby.cz/pls/ps2017nss/" + i)
-    return spojeni
-
-def rozdelene_html_obci(html_odkazy_vysledky):
-    """
-    nacte a rozdeli html jednotlivych obci
-    """
-    html_rozdelene_obce= list()
-    for odkaz in html_odkazy_vysledky:
-        prehled_obec= get(odkaz)
-        rozdelena_obec= bs(prehled_obec.text, features="html.parser")
-        html_rozdelene_obce.append(rozdelena_obec)
-    return html_rozdelene_obce
+    number_municipalities = parsered_html.find_all("td", class_="cislo")
+    codes = list()
+    for number in number_municipalities:
+        codes.append(number.string)
+    return codes
 
 
-def volici_seznam(html_rozdelene_obce):
+def find_names(parsered_html):
     """
-    vyscrapuje z html jednolivých obcí počet voličů v seznamu
+    find the name of the village, but from the main page
     """
-    pocet_volicu=list()
-    for i in html_rozdelene_obce:
-        volici= i.find_all("td", {"class":"cislo", "headers":"sa2"})
-        for i in volici:
-            pocet_volicu.append(i.string)
-    return pocet_volicu
+    names_village = parsered_html.find_all("td", {"class": "overflow_name"})
+    municipalite = list()
+    for name in names_village:
+        municipalite.append(name.string)
+    return municipalite
 
 
-def volici_obalky_hlasy(html_rozdelene_obce):
+def find_link_results(parsered_html):
     """
-    vyscrapuje z html jednotlivých obcí 
-    počet voličů v seznamu, počet vydaných obálek, počet platných hlasů
+    find links to pages with municipal election results
     """
-    pocet_volicu= list()
-    pocet_obalek= list()
-    pocet_hlasu_obec= list()
-    for i in html_rozdelene_obce:
-        volici= i.find_all("td", {"class":"cislo", "headers":"sa2"})
-        obalky= i.find_all("td", {"class":"cislo", "headers":"sa3"})
-        hlasy= i.find_all("td", {"class":"cislo", "headers":"sa6"})
-        for i in volici:
-            pocet_volicu.append(i.string)
-        for i in obalky:
-            pocet_obalek.append(i.string)
-        for i in hlasy:
-            pocet_hlasu_obec.append(i.string)
-    return pocet_hlasu_obec,pocet_obalek,pocet_volicu
+    td_with_link = parsered_html.find_all("td", {"class": "cislo"})
+    incomplete_links = list()
+    for td_tag in td_with_link:
+        a_tags = td_tag.find_all("a")
+        for a_tag in a_tags:
+            incomplete_links.append(a_tag["href"])
+    links = list()
+    for link in incomplete_links: 
+        links.append("https://volby.cz/pls/ps2017nss/" + link)
+    return links
 
 
-def strany_fce(html_strany):
+def parsering_html_municipalites(html_links_resultes):
     """
-    vyscrapuje názvy stran na seznamu (z 1.obce v seznamu),
-    ve všech obcích jsou stejné strany i jejich pořadí
+    load and parser html of municipalites
     """
-    strany=list()
-    strana= html_strany.find_all("td", {"class":"overflow_name"})
-    for i in strana:
-        strany.append(i.string)
-    return strany
+    html_parser_municipalites = list()
+    for link in html_links_resultes:
+        html_municipalites = requests.get(link)
+        parser_municipalites = bs(html_municipalites.text, features="html.parser")
+        html_parser_municipalites.append(parser_municipalites)
+    return html_parser_municipalites
+
+
+def find_electorates_envelopes_votes(html_parser_municipalites):
+    """
+    from html of municipalites find number of electorates, envelopes and votes 
+    """
+    number_electorates = list()
+    number_envelopes = list()
+    number_votes_municipalite = list()
+    for tag in html_parser_municipalites:
+        electorates = tag.find_all("td", {"class": "cislo", "headers": "sa2"})
+        envelopes = tag.find_all("td", {"class": "cislo", "headers": "sa3"})
+        votes = tag.find_all("td", {"class": "cislo", "headers": "sa6"})
+        for character in electorates:
+            number_electorates.append(character.string)
+        for character in envelopes:
+            number_envelopes.append(character.string)
+        for character in votes:
+            number_votes_municipalite.append(character.string)
+    return number_votes_municipalite, number_envelopes, number_electorates
+
+
+def find_political_party(html_parties):
+    """
+    find names of political parties (from 1st municipalite in the list),
+    (in the all municipalites are some politicat parties and their order)
+    """
+    political_parties = list()
+    polit_party = html_parties.find_all("td", {"class": "overflow_name"})
+    for character in polit_party:
+        political_parties.append(character.string)
+    return political_parties
         
-def strany_hlasy(html_rozdelene_obce):
+
+def find_numbers_political_parties(html_parser_municipalites):
+    """ 
+    finds the number of votes of all parties in all municipalities
     """
-    vyscrapuje počet hlasů všech stran ve všech obcích
+    numbers_votes_parties = list()
+    for tag in html_parser_municipalites:
+        td_tags = tag.find_all("td", {"class": "cislo", "headers": ["t1sa2 t1sb3", "t2sa2 t2sb3"]})
+        for character in td_tags:
+            numbers_votes_parties.append(character.string)
+    return numbers_votes_parties
+
+
+def parsering_html_results(municipalite, political_parties, numbers_votes_parties):
     """
-    pocty_hlasu_strany=list()
-    for i in html_rozdelene_obce:
-        hlasy_1= i.find_all("td",{"class":"cislo", "headers":["t1sa2 t1sb3","t2sa2 t2sb3"]})
-        for i in hlasy_1:
-            pocty_hlasu_strany.append(i.string)
-    return pocty_hlasu_strany
-
-def rozdeleni_vysledky_obci(obce, strany, pocty_hlasu_strany):
+    adds votes to parties by municipality 
     """
-    rozdeli hlasy jednotlivým stranám dle obcí
+    number_municipalities = len(municipalite)
+    number_parties = len(political_parties)
+    votes_party = list(list()for _ in range(number_parties))
+    index_municipality = 0
+    index_party = 0
+    for list_party in range(number_parties):
+        for _ in range(number_municipalities):
+            votes_party[list_party].append(numbers_votes_parties[(index_party*number_parties)+index_municipality])
+            index_party += 1
+        index_municipality += 1
+        index_party = 0
+    return votes_party
+    
+
+def check_url(url_address):
     """
-    pocet_obci=len(obce)
-    pocet_stran=len(strany)
-    hlasy_strany=list(list()for _ in range(pocet_stran))
-    index_obce=0
-    index_strany=0
-    for list_strany in range(pocet_stran):
-        for _ in range(pocet_obci):
-            hlasy_strany[list_strany].append(pocty_hlasu_strany[(index_strany*pocet_stran)+index_obce])
-            index_strany +=1
-        index_obce += 1
-        index_strany = 0
-    return hlasy_strany
+    checks that the URL entered is correct
+    """
+    if "ps2017" in url_address:
+        pass
+    else:
+        sys.exit("URL adresa je zadána chybně \nukončuji program")
+    try:
+        url_district = requests.get(url_address)
+    except:
+        sys.exit("URL adresa nebyla nalezena\nukončuji program")
+    if url_district.status_code == 200:
+        print("Načítám")
+    else:
+        sys.exit("Zadané URL nelze načíst\nukončuji program")
+    return url_district
 
 
+def check_csv_suffix(csv_name):
+    """
+    checks if the csv_name has a .csv suffix
+    """
+    if not csv_name.endswith(".csv"):
+        sys.exit("název csv souboru musí končit \".csv\"\nukončuji program")
 
 
-adresa= sys.argv[1]
-if "ps2017" in adresa:
-    pass
-else:
-    print("URL adresa je zadána chybně")
-    sys.exit("ukončuji program")
-
-csv_nazev= sys.argv[2]
-if not csv_nazev.endswith(".csv"):
-    print("název csv souboru musí končit \".csv\"")
-    sys.exit("ukončuji program")
-
-try:
-    prehled_okres=get(adresa)
-except:
-    print("URL adresa nebyla nalezena")
-    sys.exit("ukončuji program")
-if prehled_okres.status_code==200:
-    print("Načítám")
-else:
-    print("Zadané URL nelze načíst")
-    sys.exit("ukončuji program")
+def check_url_district(html_links_resultes):
+    """
+    checks if the link is link of district
+    """
+    if len(html_links_resultes) == 0:
+        sys.exit("Nebylo zadáno URL konkrétního okresu - výběr obce\nukončuji program")
+    else:
+        print(f"Stahuji data z vybraneho URL: {url_address}")
 
 
-rozdelene_html= bs(prehled_okres.text, features="html.parser")
-html_odkazy_vysledky= odkazy_vysledky(rozdelene_html)
-if len(html_odkazy_vysledky)==0:
-    print("Nebylo zadáno URL konkrétního okresu - výběr obce")
-    sys.exit("ukončuji program")
-else:
-    print(f"Stahuji data z vybraneho URL: {adresa}")
-kody= kod_obci(rozdelene_html)
-obce= jmena_obci(rozdelene_html)
-html_rozdelene_obce= rozdelene_html_obci(html_odkazy_vysledky)
-volici_pocet_obce= volici_seznam(html_rozdelene_obce)
-pocet_hlasu_obec, pocet_obalek, pocet_volicu = volici_obalky_hlasy(html_rozdelene_obce)
-html_strany= html_rozdelene_obce[0]
-strany= strany_fce(html_strany)
-pocty_hlasu_strany = strany_hlasy(html_rozdelene_obce)
-rozdelene_hlasy_obce= rozdeleni_vysledky_obci(obce, strany, pocty_hlasu_strany)
-
-hlavicka= ["code", "location","registered","envelopes",
+def create_csv_file(political_parties):
+    """
+    creats csv file with head and results
+    """
+    head = ["code", "location", "registered", "envelopes",
             "valid"]
-hlavicka.extend(strany)
+    head.extend(political_parties)
+    with open(csv_name, mode="w", newline="") as new_csv:
+        write_down = csv.writer(new_csv, delimiter=",")
+        print(f"Ukladam data do souboru:{csv_name}")
+        write_down.writerow(head)
+        for lines in data:
+            write_down.writerow(lines)
+    return new_csv
 
-data= zip(kody, obce, pocet_volicu, pocet_obalek, pocet_hlasu_obec, *rozdelene_hlasy_obce)
+
+url_address = sys.argv[1]
+url_district = check_url(url_address)
+
+csv_name = sys.argv[2]
+check_csv_suffix(csv_name)
+
+parsered_html = bs(url_district.text, features="html.parser")
+html_links_resultes = find_link_results(parsered_html)
+check_url_district(html_links_resultes)
+
+codes = find_code(parsered_html)
+municipalite = find_names(parsered_html)
+html_parser_municipalites = parsering_html_municipalites(html_links_resultes)
+number_votes_municipalite, number_envelopes, number_electorates = find_electorates_envelopes_votes(
+    html_parser_municipalites)
+html_parties = html_parser_municipalites[0]
+political_parties = find_political_party(html_parties)
+numbers_votes_parties = find_numbers_political_parties(html_parser_municipalites)
+parsered_votes_municipalites = parsering_html_results(municipalite, political_parties, numbers_votes_parties)
 
 
-with open(csv_nazev, mode="w", newline="") as nove_csv:
-    zapisovac= csv.writer(nove_csv, delimiter=",")
-    print(f"Ukladam data do souboru:{csv_nazev}")
-    zapisovac.writerow(hlavicka)
-    for radky in data:
-        zapisovac.writerow(radky)
-print("Ukončuji program")
+data = zip(codes, municipalite, number_electorates, number_envelopes, number_votes_municipalite,
+           *parsered_votes_municipalites)
+
+create_csv_file(political_parties)
